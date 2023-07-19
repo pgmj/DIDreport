b65c4c33f0b089ac2dbdbe5002c4a534efd7724b
Magnus Johansson <magnus.p.johansson@ri.se>
2023-07-19 10:34:51 +0200

    fix for data from Skolverket

--- skolverket.R
+++ skolverket.R
@@ -1,11 +1,15 @@
 library(tidyverse)
 library(janitor)
 
-df <- read_delim("Skolverket/Grundskolan - Antal elever per årskurs 2009 Skolenhet.csv",
+test <- read_delim("Skolverket/Grundskolan - Antal elever per årskurs 2022 Skolenhet.csv",
                  skip = 5, na = c(".",".."), delim = ";") %>%
   clean_names() %>%
   select(!x23) %>%
-  add_column(year = 2010)
+  add_column(year = 2010) %>%
+  mutate(across(starts_with("andel"), ~ gsub(",",".",.x)))
+
+
+glimpse(test)
 
 # get all filenames in the folder
 fileList <- list.files(path = "Skolverket/", full.names=TRUE)
@@ -13,19 +17,24 @@ years <- c(2010:2023)
 # create an empty list to store data in
 data <- list()
 # loop over filenames to import them to the list object
-for (i in c(1:14)) {
+for (i in c(1:length(fileList))) {
   data[[i]] <- read_delim(fileList[i],
-                          skip = 5, na = c(".",".."), delim = ";") %>%
+                          skip = 5, delim = ";") %>%
     clean_names() %>%
     select(!x23) %>%
     add_column(year = years[i]) %>%
+    mutate(across(elever_forskoleklass:elever_arskurs_9, ~ car::recode(.x,"'.'=NA;'..'=NA"))) %>%
+    mutate(across(starts_with("andel"), ~ gsub(",",".",.x))) %>%
+    mutate(across(starts_with("andel"), ~ as.numeric(.x))) %>%
     mutate(elever_arskurs_1_9 = as.numeric(elever_arskurs_1_9),
-           skol_enhetskod = as.numeric(skol_enhetskod),
-           andel_percent_elever_med_utlandsk_bakgrund_ak_1_9 = as.numeric(andel_percent_elever_med_utlandsk_bakgrund_ak_1_9))
+           skol_enhetskod = as.numeric(skol_enhetskod))
 }
 
-### Combine all nested lists into one dataframe
+
+#map(c(1:14), ~ head(data[[.x]]$andel_percent_flickor_arskurs_1_9),5)
+
+### Combine all nested dataframes into one dataframe
 df <- map_dfr(c(1:14), ~ do.call(bind_rows, data[[.x]]))
 
 library(arrow)
-write_parquet(df,"Skolverket/skolverketDemografi.parquet")
+write_parquet(df,"skolverketDemografi.parquet")
